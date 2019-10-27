40d76fa406281c1c098117ae749916d94d46ced9
Krishna <kaddala@salesforce.com>
2019-10-27 11:59:57 -0700

    initial basic outline for the gane pseudo code added

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+// Trivia questions and answer are set to 10
+// set a default times for each question or for the entire set of trivia questions to 30 seconds
+// create a question with a h2 or h3 tag and display that question
+// create buttons for each of the questions which on click should do an action
+// create an on click jquery function on that, upon click we compare the right answer  
+//upon correct or wrong answers increment the correct answers by one or the incorrect answer by 1
+// repeat this for the set of 10 questions
+// if the timer runs out, end the game and diplay the number or correct and incorrect answers
+// without page refresh we have a reset game function that we display and upon click of that we reset the game
